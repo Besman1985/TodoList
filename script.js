9c03c962da110fc8eada143857c530e89522145c
besman <besman1985@gmail.com>
2025-09-21 20:26:30 +0300

    21.09.2025

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
- 
+
   function renderToDo() {
 
     document.querySelector(".calendar-app").classList.remove("fade-out");
@@ -402,6 +402,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector(".calendar-app").classList.remove("fade-out");
     document.querySelector(".calendar-app").classList.add("fade-in");
     document.querySelector(".events-container").classList.add("weather-app");
+
     fetch("http://api.weatherapi.com/v1/forecast.json?key=ae8e436780834047a53182801251309&q=Petrozavodsk&days=7&aqi=no&alerts=no")
       .then(data => data.json())
       .then(json => {
@@ -409,6 +410,8 @@ document.addEventListener('DOMContentLoaded', () => {
         getData(json);
         dayWeatherCard(json);
       });
+
+
 
 
     // создаем класс карточек прогноза по дням
@@ -462,7 +465,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // функция для получения дня недели по индексу
     function getDayOfWeek(nam) {
       let n;
-      const week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+      const week = ["Воскресенье","Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
       week.forEach((item, index) => {
         if (nam == index) {
           n = item
@@ -668,11 +671,10 @@ document.addEventListener('DOMContentLoaded', () => {
       formData.forEach((item) => {
         localStorage.setItem(`shops:${Date.now()}`, `${item}`);
       });
-       renderByItem();
+      renderByItem();
+      addCheckStyle();
       form.reset();
     });
-
-
 
     function renderByItem() {
       document.querySelector(".ul-List").innerHTML = "";
@@ -685,27 +687,97 @@ document.addEventListener('DOMContentLoaded', () => {
           li.innerHTML = `
           <span class="item-text">${localStorage.getItem(item)}</span>
           <input type="checkbox" class="checkbox">
+          <img src="icons/delete.PNG" alt="Удалить" class="delete-icon">
           `;
-          document.querySelector(".ul-List").append(li);   
+          document.querySelector(".ul-List").append(li);
+
         };
-      }
+
+      };
+      deleteShopsItem();
     };
 
     renderByItem();
 
+    function addCheckStyle() {
+      document.querySelectorAll(".checkbox").forEach(item => {
+        item.addEventListener("click", event => {
+          event.target.parentElement.classList.toggle("check-shops");
+          checkAll();
 
 
 
+        })
+      })
+    };
+    addCheckStyle();
+
+    function checkAll() {
+      let n = 0;
+      document.querySelectorAll(".checkbox").forEach(item => {
+        if (item.checked == true) {
+          n++
+        } else {
+          n--
+        }
+
+      })
+      if (n == document.querySelectorAll(".checkbox").length) {
+        removeShopsList()
+
+      }
+    };
+
+    function removeShopsList() {
+      const div = document.createElement("div");
+      div.classList.add("container-shops-list");
+      div.classList.add("fade-in");
+      div.innerHTML = `
+      <p class="message">Вы всё купили</p>
+      <div class="buttons">
+        <button class="btn btn-delete">Удалить список</button>
+        <button class="btn btn-cancel">Отмена</button>
+      </div>
+`;
+      document.querySelector(".ul-List").prepend(div);
+      document.querySelector(".btn-delete").addEventListener("click", () => {
+        const localStorageKeys = Object.keys(localStorage);
+        for (let item of localStorageKeys) {
+          if (item.slice(0, 5) == "shops") {
+            localStorage.removeItem(item);
+          }
+        }
+        div.classList.remove("fade-in");
+        div.classList.add("fade-out");
+        setTimeout(renderByItem, 1500);
+      })
+      document.querySelector(".btn-cancel").addEventListener("click", () => {
+        div.classList.remove("fade-in");
+        div.classList.add("fade-out");
+
+        setTimeout(() => {
+          document.querySelector(".container-shops-list").remove()
+        }, 2000)
+        // renderByItem();
+        // addCheckStyle();
 
 
+      })
 
+    }
+    function deleteShopsItem() {
+      const btnDeletShops = document.querySelectorAll(".delete-icon");
+      btnDeletShops.forEach(item => {
+        item.addEventListener("click", event => {
+          if (event.target == item) {
 
-
-
-
-
-
-
+            localStorage.removeItem(item.parentElement.id);
+            renderByItem()
+            addCheckStyle()
+          }
+        })
+      });
+    }
 
 
 
@@ -784,11 +856,267 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
- function clearApp() {
+  function clearApp() {
     document.querySelector(".header").innerHTML = "";
     document.querySelector(".events-container").innerHTML = "";
     document.querySelector(".wraper-nav-cal").innerHTML = "";
   };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// general, world, nation, business, technology, entertainment, sports, science and health.
+
+
+
+
+  apikey = '4544cbf01dd600064686b8125853d3c5';
+  category = 'technology';
+  url = 'https://gnews.io/api/v4/top-headlines?category=' + category + '&lang=ru&country=rus&max=10&apikey=' + apikey;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => renderNews(data));
+
+
+
+
+
+
+
+
+
+
+  const obj = {
+
+    information: {
+      realTimeArticles: {
+        message: 'Real-time news data is only available on paid plans. Free plan has a 12-hour delay. Upgrade your plan here to remove the delay: https://gnews.io/change-plan'
+      }
+    },
+    totalArticles: 224598,
+    articles: [
+      {
+        id: '145dda464834624cd3ea459265e401ed',
+        title: 'Балтия лишилась военной поддержки США, - Reuters',
+        description: 'США намерены пересмотреть приоритеты военной помощи странам Балтии, что вызывает тревогу среди европейских дипломатов и экспертов по безопасности....',
+        content: 'В конце августа представители Пентагона на встрече с европейскими дипломатами заявили о планах ограничить поддержку в сфере безопасности Латвии, Литвы и Эстонии. Чиновник Дэвид Бейкер подчеркнул, что Европе необходимо снизить зависимость от США, а ам... [1422 chars]',
+        url: 'https://www.rbc.ua/ukr/news/tramp-skorochue-viyskovu-dopomogu-latviyi-1758422849.html',
+        image: 'https://www.rbc.ua/static/img/_/g/_gettyimages_2204103483__1__bad664a7c82a12a29584baaca6963351_1300x820.jpg',
+        publishedAt: '2025-09-21T03:10:00Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: '1139cf8acb42f9663be7d8f18a1b125c',
+        title: 'Иран заявил, что приостанавливает сотрудничество с МАГАТЭ',
+        description: 'Высший совет национальной безопасности Ирана (ВСНБ) заявил, что после необдуманных шагов, предпринятых европейскими странами против Тегерана, сотрудничество с Международным агентством по атомной энергии (МАГАТЭ) будет приостановлено.',
+        content: 'Высший совет национальной безопасности Ирана (ВСНБ) заявил, что после "необдуманных" шагов, предпринятых европейскими странами против Тегерана, сотрудничество с Международным агентством по атомной энергии (МАГАТЭ) будет приостановлено.\n' +
+          'Источник: полу... [1629 chars]',
+        url: 'https://www.pravda.com.ua/rus/news/2025/09/21/7531779/',
+        image: 'https://img.pravda.com/images/doc/7/5/7531779_fb_image_rus_2025_09_21_06_07_49.jpg',
+        publishedAt: '2025-09-21T03:04:56Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: '631bf2f616eb4e849ed91e8df4c2a193',
+        title: 'Сегодня 21 сентября: какой праздник и день в истории',
+        description: '21 сентября в Украине – Всенародный день отца и День работника леса. Также сегодня – Международный день мира',
+        content: '21 сентября в Украине – Всенародный день отца и День работника леса. Также сегодня – Международный день мира. В этот день в 2022-м российский диктатор Путин объявил «частичную мобилизацию». В 1991-м в Армении состоялся референдум за независимость и в... [4500 chars]',
+        url: 'https://www.objectiv.tv/objectively/2025/09/21/segodnya-21-sentyabrya-kakoj-prazdnik-i-den-v-istorii-3/',
+        image: 'https://www.objectiv.tv/wp-content/uploads/2025/09/21-VERESNYA-scaled.jpg',
+        publishedAt: '2025-09-21T03:00:00Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: '7b3303f579551abd2afa9f98beb1db75',
+        title: 'Великобритания введет санкции против ХАМАС вместе с признанием Палестины',
+        description: 'EADaily, 21 сентября 2025. Премьер-министр Великобритании Кир Стармер введет новые санкции против ХАМАС, параллельно намереваясь признать Палестинское государство, передает Telegraph.',
+        content: 'Премьер-министр Великобритании Кир Стармер введет новые санкции против ХАМАС, параллельно намереваясь признать Палестинское государство, передает Telegraph.\n' +
+          'Стармер планирует сделать заявление по поводу ХАМАС 21 сентября, пишет издание.\n' +
+          'Его подход бу... [836 chars]',
+        url: 'https://eadaily.com/ru/news/2025/09/21/velikobritaniya-vvedet-sankcii-protiv-hamas-vmeste-s-priznaniem-palestiny',
+        image: 'https://img5.eadaily.com/r650x400/o/407/09d602148cd305585a500ceedcb7e.jpeg',
+        publishedAt: '2025-09-21T02:50:00Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: '3121497ec7de2d214635a7a76f56ff0b',
+        title: 'Энергичность Овнов, триумф Весов и истощение Рыб: подробный гороскоп на 22 сентября',
+        description: 'Понедельник, 22 сентября, пройдет под влиянием Луны. В этот день важно сохранять внутренний баланс и не торопиться с принятием важных решений. Энергия дня поможет лучше понять собственные чувства и мягко начать рабочую неделю, пишет ИА PrimaMedia.',
+        content: 'Понедельник, 22 сентября, пройдет под влиянием Луны. В этот день важно сохранять внутренний баланс и не торопиться с принятием важных решений. Энергия дня поможет лучше понять собственные чувства и мягко начать рабочую неделю, пишет ИА PrimaMedia.\n' +
+          'Ов... [1969 chars]',
+        url: 'https://primamedia.ru/news/2228547/',
+        image: 'https://primamedia.ru/f/main/5632/5631943.png?609e26fa5c98c8c2c300a2e32697308d',
+        publishedAt: '2025-09-21T02:09:11Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: 'e5c9f8ff410ddfeb62612480340ebdb7',
+        title: 'Финансовая удача в конце сентября: три знака зодиака получат неожиданные доходы',
+        description: 'Конец сентября принесет с собой мощные финансовые изменения, которые коснутся сразу нескольких знаков зодиака. Астрологи отмечают, что этот период станет временем приятных сюрпризов, денежных поступлений и выгодных возможностей.',
+        content: 'Конец сентября принесет с собой мощные финансовые изменения, которые коснутся сразу нескольких знаков зодиака. Астрологи отмечают, что этот период станет временем приятных сюрпризов, денежных поступлений и выгодных возможностей. Звезды откроют двери ... [1944 chars]',
+        url: 'https://www.obozrevatel.com/novosti-obschestvo/finansovaya-udacha-v-kontse-sentyabrya-tri-znaka-zodiaka-poluchat-neozhidannyie-dohodyi.htm',
+        image: 'https://i.obozrevatel.com/news/2024/7/25/50eb7fe4-04ce-4188-a721-0395ffa9696b.png?size=1200x630',
+        publishedAt: '2025-09-21T02:03:00Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: '5292e6105c3f953942a73b77328dbc9d',
+        title: 'В России жестко отреагировали на призыв экс-украинских властей "идти на Москву"',
+        description: 'Ранее на Украине призвали «идти на Москву»',
+        content: 'Ранее на Украине призвали «идти на Москву» Фото: Создано в Midjourney © URA.RU\n' +
+          'новость из сюжета\n' +
+          'Спецоперация РФ на Украине\n' +
+          'Бывший президент Украины Виктор Ющенко — ужасный трус и предатель. Об этом сообщил депутат Госдумы от Крыма Михаил Шеремет, ко... [3957 chars]',
+        url: 'https://ura.news/news/1052998892',
+        image: 'https://s.ura.news/images/news/figures/591/514/520e0c47-fd5f-4867-8040-7a66463d4542/zrk0il/L-1200.1.9.jpg',
+        publishedAt: '2025-09-21T01:38:50Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: 'ab0831938fc77c43910bbea9d3fcc917',
+        title: 'Народные методы лечения ОРВИ: какие оправданы, а какие опасны - 21 сентября 2025',
+        description: 'Наступивший сезон простуд и ОРВИ для многих — повод вспомнить бесценные бабушкины рецепты, с помощью которых нас когда-то - Здоровье - 21 сентября 2025 | 74.ру',
+        content: 'Наступивший сезон простуд и ОРВИ для многих — повод вспомнить бесценные бабушкины рецепты, с помощью которых нас когда-то лечили в детстве. Кому не сыпали в носки горчицу и не рисовали на груди йодную сеточку? Мы обсудили эффективность таких методов ... [5745 chars]',
+        url: 'https://74.ru/text/health/2025/09/21/76037027/',
+        image: 'https://74.ru/html-to-img/og/MNSZF57qFxyw2CHr4X7Gwg/article-id6561353-16x9.jpg?1758363118',
+        publishedAt: '2025-09-21T01:30:00Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: 'aef68f836288c1746237b1bc08c6bc58',
+        title: 'Трамп хочет вернуть авиабазу Bagram и пригрозил Афганистану',
+        description: 'Президент США Дональд Трамп заявил, что случятся плохие вещи, если Афганистан не вернет Штатам авиабазу Bagram.',
+        content: 'Президент США Дональд Трамп заявил, что случятся "плохие вещи", если Афганистан не вернет Штатам авиабазу Bagram.\n' +
+          'Источник: Трамп в своей соцсети Truth Social, "Радио Свобода"\n' +
+          'Прямая речь Трампа: "Если Афганистан не вернет авиабазу Bagram тем, кто ее... [1105 chars]',
+        url: 'https://www.pravda.com.ua/rus/news/2025/09/21/7531777/',
+        image: 'https://img.pravda.com/images/doc/7/5/7531777_fb_image_rus_2025_09_21_04_13_54.jpg',
+        publishedAt: '2025-09-21T01:08:56Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      },
+      {
+        id: 'ae93d9bd50190516336ef46103abd3ff',
+        title: 'Вучич заявил об "охоте" Запада из-за новостей о приезде Лаврова',
+        description: 'Западные страны сочли скандалом информацию о возможном приезде в Белград министра иностранных дел России Сергея Лаврова на военный парад 20 сентября. Об этом сообщил президент Сербии Александр Вучич, передает РБК',
+        content: 'Западные страны сочли скандалом информацию о возможном приезде в Белград министра иностранных дел России Сергея Лаврова на военный парад 20 сентября. Об этом сообщил президент Сербии Александр Вучич, передает РБК.\n' +
+          'По его словам, из-за незнания лидеро... [322 chars]',
+        url: 'https://tve24.ru/news/2025/09/21/vuchich-zayavil-ob-ohote-zapada-iz-za-novostey-o-priezde-lavrova/',
+        image: 'https://icdn.lenta.ru/images/2025/09/21/04/20250921040753801/share_ee636589bc3f931a5b39a239db5d0c37.jpg',
+        publishedAt: '2025-09-21T01:08:00Z',
+        lang: 'ru',
+        source: {
+          id: '6f1ead09126f1f4738a48957e22f789c',
+          name: 'РБК-Україна',
+          url: 'https://www.rbc.ua',
+          country: 'ua'
+        }
+      }
+    ]
+
+  };
+
+
+
+
+  function renderNews(responce) {
+    const articles = Object.values(responce.articles);
+    for (let value of articles) {
+      let { title, description, content, url, image } = value;
+      console.log(content);
+    }
+
+  };
+
+
+
+  renderNews(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
